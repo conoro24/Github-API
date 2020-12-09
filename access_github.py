89522f8baee8a6aac7d9c9dfc180a77f8b780237
Conor O'Neill <60669583+conoro24@users.noreply.github.com>
2020-12-09 14:22:32 -0500

    Update access token

--- access_github.py
+++ access_github.py
@@ -1,7 +1,7 @@
 
 from github import Github
 
-g = Github("a343260157823bdbf95311f60966c2ca8b638ea5")
+g = Github("7e760a69e55d4cf0c3513412d672c778b2dce87b")
 
 
 for repo in g.get_user().get_repos():
