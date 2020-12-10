98be7f6993c4de15210581862b814c9e880ae35f
Conor O'Neill <60669583+conoro24@users.noreply.github.com>
2020-12-10 14:43:13 -0500

    Printing Repos for my username

--- access_github.py
+++ access_github.py
@@ -1,11 +1,16 @@
 
-import requests
+import base64
+from github import Github
 from pprint import pprint
 
 username = 'conoro24'
-url = 'https://api.github.com/users/conoro24'
+g = Github()
 
-user_data = requests.get(url).json()
-pprint(user_data)
+user = g.get_user(username)
+for repo in user.get_repos():
+	pprint(repo)
+
+
+
 
 
