0759ac727a51a1981d5d7b43a93454ef01dbfc0e
Conor O'Neill <60669583+conoro24@users.noreply.github.com>
2020-12-11 11:21:11 -0500

    Getting descriptions and info of repos

--- access_github.py
+++ access_github.py
@@ -1,4 +1,5 @@
 
+
 import base64
 from github import Github
 from pprint import pprint
@@ -6,10 +7,26 @@ from pprint import pprint
 username = 'conoro24'
 g = Github()
 
+
+def print_repo(repo):
+
+    print("Full name:", repo.full_name)
+    print("Description:", repo.description)
+    print("Date created:", repo.created_at)
+    print("Date of last push:", repo.pushed_at)
+    print("Home Page:", repo.homepage)
+    print("Language:", repo.language)
+    print("Contents:")
+    for content in repo.get_contents(""):
+        print(content)
+    try:
+        print("License:", base64.b64decode(repo.get_license().content.encode()).decode())
+    except:
+        pass
+
 user = g.get_user(username)
 for repo in user.get_repos():
-	pprint(repo)
-
+	print_repo(repo)
 
 
 
